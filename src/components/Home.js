1272859262f7ef15f1ca4a233835ad544bae73e8
Eliezer Bittencourt <likobittencourt@gmail.com>
2023-09-29 21:02:01 -0300

    features: add components

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Artigos Recentes</h2>
+      {/* Lista de artigos aqui */}
+    </div>
+  );
+}
+
+export default Home;
